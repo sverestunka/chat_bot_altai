4e1420868f74debb21e994a7faff5b4d99b24acf
Svetlana_Verestun <sverestunka@mail.ru>
2024-06-23 22:57:18 +0300

    Нерабочая версия1

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, fsm, handlers
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
+
 import config
 API_TOKEN = config.token
 # Включаем логирование, чтобы видеть сообщения в консоли
 logging.basicConfig(level=logging.INFO)
 # Инициализация бота и диспетчера
-bot = Bot(token=API_TOKEN)
+bot: Bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def send_welcome(message: types.Message):
-    await message.answer("Привет! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.")
+async def cmd_start(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f"Привет, {name}", reply_markup=keyboard2)
 
 @dp.message(Command("help"))
 async def command_help(message: types.Message):
@@ -21,7 +26,7 @@ async def command_help(message: types.Message):
 
 @dp.message(Command("info"))
 async def command_info(message: types.Message):
-    await message.answer("Я бот, созданный для помощи вам. Я могу отвечать на ваши вопросы, помогать вам узнавать новую информацию и многое другое. Просто напишите мне!")
+    await message.answer("Я бот, могу повторять за тобой")
 
 @dp.message()
 async def echo(message: types.Message):
