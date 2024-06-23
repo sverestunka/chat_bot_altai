64fc5e77a0cdfad5e1025696ab659d25fca88178
Svetlana_Verestun <sverestunka@mail.ru>
2024-06-23 14:23:54 +0300

    Исходная версия бота

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import config
+API_TOKEN = config.token
+# Включаем логирование, чтобы видеть сообщения в консоли
+logging.basicConfig(level=logging.INFO)
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def send_welcome(message: types.Message):
+    await message.answer("Привет! Я эхобот на aiogram 3. Отправь мне любое сообщение, и я повторю его.")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
