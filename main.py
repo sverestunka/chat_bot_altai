aa32c04e8c0290fd87f9e32443ecc7e452404453
Svetlana_Verestun <sverestunka@mail.ru>
2024-06-24 02:26:05 +0300

    Рабочая версия. без повторов. Новые фильтры (start! stop! Ответ по имени на приветствие hello и прощание by)

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, types, fsm, handlers
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.command import Command
+from aiogram.filters import CommandObject
 
 import config
 API_TOKEN = config.token
@@ -15,10 +17,22 @@ logging.basicConfig(level=logging.INFO)
 bot: Bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f"Привет, {name}", reply_markup=keyboard2)
+"""button1 = types.KeyboardButton(text='info')
+button2 = types.KeyboardButton(text='help')
+kb = [
+    [button1, button2]
+]
+keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)"""
+
+@dp.message(F.text.lower() == "stop!")
+async def with_puree(message: types.Message):
+    await message.reply("Стоп машина!!")
+@dp.message(F.text.lower() == "start!")
+async def without_puree(message: types.Message):
+    await message.reply("Поехали!")
+@dp.message(F.animation)
+async def echo_gif(message: types.Message):
+    await message.reply_animation(message.animation.file_id)
 
 @dp.message(Command("help"))
 async def command_help(message: types.Message):
@@ -27,6 +41,20 @@ async def command_help(message: types.Message):
 @dp.message(Command("info"))
 async def command_info(message: types.Message):
     await message.answer("Я бот, могу повторять за тобой")
+
+
+@dp.message(F.text)
+async def msg_echo(message: types.Message):
+    print(message.text)
+    name = message.chat.first_name
+    if 'hello' in message.text.lower():
+        await message.reply(f"И тебе привет, {name}")
+    if 'by' in message.text.lower():
+        await message.reply(f"Пока, {name}")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 @dp.message()
 async def echo(message: types.Message):
